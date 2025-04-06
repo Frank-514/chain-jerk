ac3bd960d0112e1c65228bb18e02ca350515d528
Frank-514 <frank373859050@gmail.com>
2025-04-06 19:14:38 +0800

    Day1: 添加身份卡程序

---
+++ c1.cpp
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main()
+{	char name[20];
+	char number[10];
+	char profession[20];
+	printf("欢迎进入素拓分积分系统\n");
+	printf("请输入姓名：");
+	scanf("%s",name);
+	printf("请输入学号：");
+	scanf("%s",number);
+	printf("请输入专业：");
+	scanf("%s",profession);
+	printf("=====区块链数字身份卡=====\n");
+	printf("姓名：%s\n",name);
+	printf("学号：%s\n",number);
+	printf("专业：%s\n",profession);
+
+
+
+	
+	
+	
+	
+	return 0;
+
+
+
+
+
+
+
+
+
+
+}
